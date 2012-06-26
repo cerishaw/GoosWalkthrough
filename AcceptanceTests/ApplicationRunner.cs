b5b44f023b851e5e47e7cd302999a2c4a0e969ab
Ceri Shaw <mrs.cshaw@gmail.com>
2012-06-26 14:19:19 +0100

    First stab at the fakeAuctionServer

--- AcceptanceTests/ApplicationRunner.cs
+++ AcceptanceTests/ApplicationRunner.cs
@@ -1,20 +1,39 @@
+using System.Threading;
+
 namespace AcceptanceTests
 {
-    internal class ApplicationRunner    
+    internal class ApplicationRunner
     {
-        public void Stop()
-        {
-            throw new System.NotImplementedException();
-        }
+        public const string SniperId = "sniper";
+        public const string SniperPassword = "sniper";
+//        private AuctionSniperDriver driver;
+
 
         public void StartBiddingIn(FakeAuctionServer auction)
         {
-            throw new System.NotImplementedException();
+            var thread = new Thread(StartApplication);
+            thread.Start();
+//            driver = new AuctionSniperDriver(1000);
+//            driver.ShowsSniperStatus(StatusJoining);
         }
 
         public void ShowsSniperHasLostAuction()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void Stop()
+        {
+//            if (driver != null)
+//            {
+//                driver.Dispose();
+//            }
+        }
+
+        private void StartApplication()
+        {
+            AuctionSniper.App.Main();
+
         }
     }
 }
--- AcceptanceTests/FakeAuctionServer.cs
+++ AcceptanceTests/FakeAuctionServer.cs
@@ -1,30 +1,75 @@
+using System.Collections.Concurrent;
+using Matrix;
+using Matrix.Xmpp;
+using Matrix.Xmpp.Client;
+using NUnit.Framework;
+
 namespace AcceptanceTests
 {
-    internal class FakeAuctionServer    
+    internal class FakeAuctionServer
     {
-        public FakeAuctionServer(string item)
-        {
-            throw new System.NotImplementedException();
-        }
+        public const string ItemIdAsLogin = "auction-%s";
+        public const string AuctionResource = "Auction";
+        public const string XmppHostName = "localhost";
+        private const string AuctionPassword = "auction";
 
-        public void Stop()
+        private readonly string itemId;
+        private readonly XmppClient xmppClient;
+        private readonly SingleMessageListener messageListener = new SingleMessageListener();
+        private Jid chatId;
+
+        public FakeAuctionServer(string itemId)
         {
-            throw new System.NotImplementedException();
+            this.itemId = itemId;
+            xmppClient = new XmppClient {XmppDomain = XmppHostName};
         }
 
         public void StartSellingItem()
         {
-            throw new System.NotImplementedException();
+            xmppClient.SetUsername(string.Format(ItemIdAsLogin, itemId));
+            xmppClient.Password = AuctionPassword;
+            xmppClient.SetResource(AuctionResource);
+            xmppClient.OnMessage += xmppClient_OnMessage;
+            xmppClient.Open();
+        }
+
+        private void xmppClient_OnMessage(object sender, MessageEventArgs e)
+        {
+            chatId = e.Message.From;
+            messageListener.ProcessMessage(e.Message);
         }
 
         public void HasReceivedJoinRequestFromSniper()
         {
-            throw new System.NotImplementedException();
+            messageListener.ReceivesAMessage();
         }
 
         public void AnnounceClose()
         {
-            throw new System.NotImplementedException();
+            xmppClient.Send(new Message(chatId, MessageType.chat, ""));
+        }
+
+        public void Stop()
+        {
+            xmppClient.Close();
+        }
+
+        private class SingleMessageListener
+        {
+            private readonly BlockingCollection<Message> messages = new BlockingCollection<Message>(1);
+
+            public void ProcessMessage(Message message)
+            {
+                messages.Add(message);
+            }
+
+            public void ReceivesAMessage()
+            {
+                Message result;
+                const int timeout = 5000; //5 seconds
+                messages.TryTake(out result, timeout);
+                Assert.That(result, Is.Not.Null);
+            }
         }
     }
 }
